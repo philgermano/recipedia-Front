bd02fa9ee87883833a86b666c7758757d7f70321
clopezferrer <lopezferrer@gmail.com>
2022-10-07 19:56:15 -0400

    about styling

--- src/routes/AboutNav.jsx
+++ src/routes/AboutNav.jsx
@@ -8,7 +8,7 @@ const AboutNav=()=>{
     return <div className="about-nav">
         <ul className="dev-team">
             <li>
-                <div className="dev-team-card" >
+                <div className={location.pathname==='/about/carlos'?'dev_active':'dev_inactive'} >
                     <Link to="/about/carlos" className={location.pathname==='/about/carlos'?'home_active':'home_inactive'}  style={{textDecoration: 'none', color:'black'}} >
                         <p className="names">Carlos</p>
                         <img className="picture" src={require('../img/carlos.jpeg')} />
@@ -16,7 +16,7 @@ const AboutNav=()=>{
                 </div>
             </li>
             <li>
-                <div className="dev-team-card" >
+                <div className={location.pathname==='/about/clara'?'dev_active':'dev_inactive'} >
                     <Link to="/about/clara" className={location.pathname==='/about/clara'?'home_active':'home_inactive'} style={{textDecoration: 'none', color:'black'}} >
                         <p className="names">Clara</p>
                         <img className="picture" src={require('../img/clara.jpg')} />
@@ -24,7 +24,7 @@ const AboutNav=()=>{
                 </div>
             </li>
             <li>
-                <div className="dev-team-card" >
+                <div className={location.pathname==='/about/phil'?'dev_active':'dev_inactive'}>
                     <Link to="/about/phil" className={location.pathname==='/about/phil'?'home_active':'home_inactive'} style={{textDecoration: 'none', color:'black'}} >
                         <p className="names">Phil</p>
                         <img className="picture" src={require('../img/phil.jpg')} />
